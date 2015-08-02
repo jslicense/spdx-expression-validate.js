081755e6999716f1bfb0aa7ebea964ae6f58042d
Kyle Mitchell <kyle@kemitchell.com>
2015-08-01 20:50:15 -0700

    Reduce to spdx-expression-validate

--- index.js
+++ index.js
@@ -1,31 +1,18 @@
-var parser = require('./parser.generated.js').parser;
-
-exports.parse = function(argument) {
-  return parser.parse(argument);
-};
+var parse = require('spdx-expression-parse')
 
 var containsRepeatedSpace = /\s{2,}/;
 
-exports.valid = function(argument) {
-  if (
+function spdxExpressionValidate(argument) {
+  var fatString = (
     argument.trim() !== argument ||
-    containsRepeatedSpace.test(argument)
-  ) {
-    return false;
-  }
-  try {
-    parser.parse(argument);
-    return true;
-  } catch (e) {
-    // jison generates parsers that throw errors, while this function
-    // mimics `semver.valid` by returning null.
-    return null;
-  }
-};
+    containsRepeatedSpace.test(argument) )
+  if (fatString) {
+    return false }
+  else {
+    try {
+      parse(argument)
+      return true }
+    catch (e) {
+      return false } } }
 
-// Require the same license and exception data used by the parser.
-exports.licenses = require('spdx-license-ids');
-exports.exceptions = require('spdx-exceptions');
-
-// The License Expression Syntax version
-exports.specificationVersion = '2.0';
+module.exports = spdxExpressionValidate
